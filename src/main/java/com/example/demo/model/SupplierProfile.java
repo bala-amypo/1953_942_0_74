e6dd385ccc3abc88cd6428bd4ebf129a1ef3a09f
bala-amypo <bala@amypo.in>
2025-12-17 08:28:14 +0000

    User commit

--- src/main/java/com/example/demo/model/SupplierProfile.java
+++ src/main/java/com/example/demo/model/SupplierProfile.java
@@ -1,7 +1,12 @@
 package com.example.demo.model;
 
+
+@Entity
 public class SupplierProfile{
+
+    @Id
     private Long id;
+    
     private String supplierCode;
     private String supplierName;
     private String email;
