6a02ce1dea53614dbe1ba06afa825a05e56f2910
bala-amypo <bala@amypo.in>
2025-12-17 08:44:42 +0000

    User commit

--- src/main/java/com/example/demo/entity/Stuentity.java
+++ src/main/java/com/example/demo/entity/Stuentity.java
@@ -6,8 +6,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 
-
-
 @Entity
 public class Stuentity{
     @Id
--- src/main/java/com/example/demo/model/SupplierProfile.java
+++ src/main/java/com/example/demo/model/SupplierProfile.java
@@ -1,12 +1,18 @@
 package com.example.demo.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 
 @Entity
 public class SupplierProfile{
 
     @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Long id;
-    
+    @Column(unique=true)
     private String supplierCode;
     private String supplierName;
     private String email;
